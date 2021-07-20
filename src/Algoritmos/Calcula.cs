9a8937ef2b9baed0ea412d55611de8d407d9c5ba
Rayans Carvalho Santos <rayanscs@gmail.com>
2021-07-20 19:27:12 -0300

    projeto

---
+++ src/Algoritmos/Calcula.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Algoritmos
+{
+    public class Calcula
+    {
+        public int VerificaMaiorNumero(List<int> lista)
+        {
+            var maior = 0;
+
+            for (int i = 0; i < lista.Count; i++)
+            {
+                var item = lista[i];
+
+                if (item > maior)
+                {
+                    maior = item;
+                }
+            }
+
+            return maior;
+        }
+
+        public string VerificaMaiorNumeroMelhor(List<int> lista)
+        { 
+            return $@"MAIOR:{lista.Max()}  MENOR:{lista.Min()}";
+        }
+
+        public int VerificaMenorNumeroMelhor(List<int> lista)
+        {
+            return lista.Min();
+        }
+
+        public int VerificaMenorNumero(List<int> lista)
+        {
+            int max = int.MaxValue;
+
+            for (int i = 0; i < lista.Count; i++)
+            {
+                var item = lista[i];
+
+                if (item < max)
+                {
+                    max = item;
+                }
+            }
+
+            return max;
+        }
+
+
+
+
+    }
+}
---
+++ src/Algoritmos/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algoritmos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> inteiros = new List<int>() { 30, 4, 10, -2, 3, 6, 8 };
+
+            var calcula = new Calcula();
+            var maior = calcula.VerificaMaiorNumero(inteiros);
+            var menor = calcula.VerificaMenorNumero(inteiros);
+
+            Console.WriteLine($@"O maior numero é: {maior}");
+            Console.WriteLine($@"O menor numero é: {menor}");
+
+            var juntos = calcula.VerificaMaiorNumeroMelhor(inteiros);
+
+            Console.WriteLine($@"{juntos}");
+        }
+    }
+}
